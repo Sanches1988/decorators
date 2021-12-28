9c2d8c2ec01c7fc134ef8f68ad8512a357b97765
Александр Пирожков <sanchellopirozhkov@mail.ru>
2021-12-28 11:17:01 +0300

    initial commit

---
+++ first_task.py
@@ -0,0 +1,26 @@
+from datetime import datetime as dt
+import requests
+
+
+def get_log(func):
+    def foo(*args, **kwargs):
+        date_time = dt.now()
+        func_name = func.__name__
+        result = func(*args, **kwargs)
+        with open('logs.txt', 'w', encoding='utf-8') as file:
+            file.write(f'Дата и время вызова функции: {date_time}\n'
+                       f'Имя функции: {func_name}\n'
+                       f'Аргумент: {args, kwargs}\n'
+                       f'Результат: {result}\n')
+        return result
+    return foo
+
+@get_log
+def get_status(*args, **kwargs):
+    url = ','.join(args)
+    response = requests.get(url=url)
+    return response.status_code
+
+
+if __name__ == '__main__':
+    get_status('https://www.championat.com/football/')
---
+++ third_task.py
@@ -0,0 +1,30 @@
+from datetime import datetime as dt
+
+nested_list = [
+	['a', 'b', 'c'],
+	['d', 'e', 'f'],
+	[1, 2, None],
+]
+
+def get_log(func):
+    def foo(*args, **kwargs):
+        date_time = dt.now()
+        func_name = func.__name__
+        result = func(*args, **kwargs)
+        with open('logs.txt', 'w', encoding='utf-8') as file:
+            file.write(f'Дата/время: {date_time}\n'
+                       f'Имя функции: {func_name}\n'
+                       f'Аргументы: {args, kwargs}\n'
+                       f'Результат: {result}\n')
+        return result
+    return foo
+
+
+@get_log
+def generator(nested_list):
+    for i in nested_list:
+        for y in i:
+            yield y
+
+for item in generator(nested_list):
+    print(item)
